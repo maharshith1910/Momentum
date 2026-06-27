b62ef2f6e26e74a2e4601d49e1e3d73556f53353
maharshith1910 <maharshithn@gmail.com>
2026-06-27 16:59:03 +0530

    feat : initialized Spring Boot backend and health endpoint

---
+++ backend/src/test/java/com/maharshith/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharshith.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
